5fc13728338c22de89e74c31ba7e7935efb45161
LEGEND X <74249238+legendx22@users.noreply.github.com>
2021-01-09 20:08:48 +0530

    Fix kro saar

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -142,7 +142,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/@friendskailaka")],
+                                                                       url="t.me/friendskailaka")],
                                                                                    [InlineKeyboardButton(text="ADD MADBOI BOT TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
